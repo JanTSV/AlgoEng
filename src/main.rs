aa9d28775fcef71ded82048de4ae77e25c088c3f
JanTSV <jantheil2001@gmail.com>
2025-04-07 23:57:27 +0200

    offsetarrays

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::error::Error;
 
-fn parse_graph(filename: &str) -> Result<(), Box<dyn Error>> {
+type Edge = (u64, u64, u64);
+type OffsetArray = (Vec<Edge>, Vec<u64>);
+
+fn parse_graph(filename: &str) -> Result<(OffsetArray, OffsetArray), Box<dyn Error>> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
 
@@ -17,30 +20,23 @@ fn parse_graph(filename: &str) -> Result<(), Box<dyn Error>> {
     let num_nodes: u64 = lines.next().ok_or("Missing number of nodes")?.parse()?;
     let num_edges: u64 = lines.next().ok_or("Missing number of edges")?.parse()?;
 
-    // Read the nodes
-    for _ in 0..num_nodes {
-        let line = lines.next().ok_or("Missing node line")?;
-        let parts: Vec<&str> = line.split_whitespace().collect();
-        let id: u64 = parts[0].parse()?;
-        assert!(id < num_nodes, "Node ID is bigger than num_nodes");
-        // let _osm_id : u64 = parts[1].parse()?;
-        // let _lat : f64 = parts[2].parse()?;
-        // let _lon : f64 = parts[3].parse()?;
-        // let _height : u64 = parts[4].parse()?;
-    }
+    // Risky assumption: all nodes are defined in order and gap less
+    lines.nth((num_nodes - 1) as usize).ok_or("Missing node lines")?;
 
     // Read the edges
+    let mut outgoing_edges: Vec<Vec<Edge>> = vec![Vec::new(); num_nodes as usize];
+    let mut incoming_edges: Vec<Vec<Edge>> = vec![Vec::new(); num_nodes as usize];
     for _ in 0..num_edges {
         let line = lines.next().ok_or("Missing edge line")?;
         let parts: Vec<&str> = line.split_whitespace().collect();
         let source : u64 = parts[0].parse()?;
         let target : u64 = parts[1].parse()?;
         let weight : u64 = parts[2].parse()?;
-        // let _edge_type : u64 = parts[3].parse()?;
-        // let _max_speed : u64 = parts[4].parse()?;
+        outgoing_edges[source as usize].push((source, target, weight));
+        incoming_edges[target as usize].push((source, target, weight));
     }
 
-    // TODO: Return the offset-arrays
+    // Return the offset-arrays
     // Following data structures are needed:
     // Edges:                   Array-Offsets
     // (src, target, distance)  (Node, EndIdx)
@@ -50,7 +46,32 @@ fn parse_graph(filename: &str) -> Result<(), Box<dyn Error>> {
     // (2, 3, 5)                ...
     // (2, 89, 1)               (N, M)
     // ....
-    Ok(())
+    let mut flat_outgoing_edges: Vec<Edge> = Vec::with_capacity(num_edges as usize);
+    let mut outgoing_offsets: Vec<u64> = Vec::with_capacity(num_nodes as usize + 1);
+    
+    let mut current_offset = 0;
+    
+    for edges_for_node in outgoing_edges {
+        current_offset += edges_for_node.len() as u64;
+        flat_outgoing_edges.extend(edges_for_node);
+        outgoing_offsets.push(current_offset);
+    }
+    outgoing_offsets.push(current_offset);
+
+    let mut flat_incoming_edges: Vec<Edge> = Vec::with_capacity(num_edges as usize);
+    let mut incoming_offsets: Vec<u64> = Vec::with_capacity(num_nodes as usize + 1);
+    
+    let mut current_offset = 0;
+    
+    for edges_for_node in incoming_edges {
+        current_offset += edges_for_node.len() as u64;
+        flat_incoming_edges.extend(edges_for_node);
+        incoming_offsets.push(current_offset);
+    }
+    incoming_offsets.push(current_offset);
+
+
+    Ok(((flat_outgoing_edges, outgoing_offsets), (flat_incoming_edges, incoming_offsets)))
 }
 
 

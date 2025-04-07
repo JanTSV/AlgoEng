fa03a3185fd776fe14915b2bb6f115c8cf0961ee
JanTSV <jantheil2001@gmail.com>
2025-04-07 22:55:14 +0200

    TODO: offset arrays

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::error::Error;
 
-#[derive(Debug)]
-struct Node {
-    id: u64,
-    osm_id: u64,
-    lat: f64,
-    lon: f64,
-    height: f64,
-}
-
-#[derive(Debug)]
-struct Edge {
-    source: u64,
-    target: u64,
-    weight: u64,
-    edge_type: u64,
-    max_speed: u64,
-}
-
-fn parse_graph(filename: &str) -> Result<(Vec<Node>, Vec<Edge>), Box<dyn Error>> {
+fn parse_graph(filename: &str) -> Result<(), Box<dyn Error>> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
 
@@ -32,46 +14,48 @@ fn parse_graph(filename: &str) -> Result<(Vec<Node>, Vec<Edge>), Box<dyn Error>>
         .filter(|line| !line.is_empty() && !line.starts_with('#'));
 
     // Read the first 2 lines consisting of the number of nodes and the number of edges
-    let num_nodes: usize = lines.next().ok_or("Missing number of nodes")?.parse()?;
-    let num_edges: usize = lines.next().ok_or("Missing number of edges")?.parse()?;
+    let num_nodes: u64 = lines.next().ok_or("Missing number of nodes")?.parse()?;
+    let num_edges: u64 = lines.next().ok_or("Missing number of edges")?.parse()?;
 
     // Read the nodes
-    let mut nodes = Vec::with_capacity(num_nodes);
     for _ in 0..num_nodes {
         let line = lines.next().ok_or("Missing node line")?;
         let parts: Vec<&str> = line.split_whitespace().collect();
-        let node = Node {
-            id: parts[0].parse()?,
-            osm_id: parts[1].parse()?,
-            lat: parts[2].parse()?,
-            lon: parts[3].parse()?,
-            height: parts[4].parse()?,
-        };
-        nodes.push(node);
+        let id: u64 = parts[0].parse()?;
+        assert!(id < num_nodes, "Node ID is bigger than num_nodes");
+        // let _osm_id : u64 = parts[1].parse()?;
+        // let _lat : f64 = parts[2].parse()?;
+        // let _lon : f64 = parts[3].parse()?;
+        // let _height : u64 = parts[4].parse()?;
     }
 
     // Read the edges
-    let mut edges = Vec::with_capacity(num_edges);
     for _ in 0..num_edges {
         let line = lines.next().ok_or("Missing edge line")?;
         let parts: Vec<&str> = line.split_whitespace().collect();
-        let edge = Edge {
-            source: parts[0].parse()?,
-            target: parts[1].parse()?,
-            weight: parts[2].parse()?,
-            edge_type: parts[3].parse()?,
-            max_speed: parts[4].parse()?,
-        };
-        edges.push(edge);
+        let source : u64 = parts[0].parse()?;
+        let target : u64 = parts[1].parse()?;
+        let weight : u64 = parts[2].parse()?;
+        // let _edge_type : u64 = parts[3].parse()?;
+        // let _max_speed : u64 = parts[4].parse()?;
     }
 
     // TODO: Return the offset-arrays
-    Ok((nodes, edges))
+    // Following data structures are needed:
+    // Edges:                   Array-Offsets
+    // (src, target, distance)  (Node, EndIdx)
+    // (0, 1, 2)                (0, 2)
+    // (0, 3, 12)               (1, 4 -> from 2)
+    // (0, 15, 78)              (2, 4)
+    // (2, 3, 5)                ...
+    // (2, 89, 1)               (N, M)
+    // ....
+    Ok(())
 }
 
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let (nodes, edges) = parse_graph("inputs/MV.fmi")?;
+    parse_graph("inputs/MV.fmi")?;
 
     Ok(())
 }

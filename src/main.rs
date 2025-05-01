f322ef7010b49812611ece67edb1f8a73b99cebe
JanTSV <jantheil2001@gmail.com>
2025-05-01 13:29:00 +0200

    working on node contraction

--- src/main.rs
+++ src/main.rs
@@ -103,7 +103,7 @@ fn parse_graph(filename: &str) -> Result<(Vec<u64>, OffsetArray, OffsetArray), B
 
         let source: u64 = levels[parts[0].parse::<usize>()?].0;
         let target: u64 = levels[parts[1].parse::<usize>()?].0;
-        let weight: u64 = levels[parts[2].parse::<usize>()?].0;
+        let weight: u64 = parts[2].parse()?;
         let edge_id_a: Option<u64> = if parts.len() > 5 {
             parts[5].parse().ok()
         } else {
@@ -325,60 +325,96 @@ impl<'a> CH<'a> {
         distance
     }
 
-    pub fn preprocess(graph: &mut OffsetArray) {
+    pub fn preprocess(graph: &mut OffsetArray, incoming_graph: &mut OffsetArray, perm: &Vec<u64>) {
         // TODO: Preprocessing needs to be called on every weak component
         // Use dfs to reach weak components and call sub function on this comp
         // dfs() needs to return list of nodes in component (Independent set)
         // Sort independent set by edge-difference (#shortcuts created - #edges deleted)
         // Contract nodes (u) in that order with Dijkstras (shortcuts only if Dijkstra(neighbor_i, neighbor_j)
         //  > c(u, neighbor_i) + c(u, neighbor_j). i and j are edgeA and edgeB
+        let mut visited = vec![false; graph.1.len() - 1];
 
-    }
-}
+        for node in 0..visited.len() {
+            if !visited[node] {
+                let mut subgraph = Self::dfs(graph, incoming_graph, node as u64, &mut visited);
 
-fn dfs(graph: &OffsetArray, incoming_graph: &OffsetArray, start: u64, visited: &mut [bool]) {
-    let mut stack = Vec::new();
-    stack.push(start);
+                // Sort subgraph by increasing #shortcuts created - #edges deleted
+                subgraph.sort_by_key(|node| {
+                    let edges_deleted = Self::calc_edges_deleted(*node, graph, incoming_graph);
+                    let shortcuts_created = Self::calc_shortcuts(*node, graph, incoming_graph).len();
+                    
+                    shortcuts_created as i64 - edges_deleted as i64
+                });
 
-    while let Some(node) = stack.pop() {
-        let node = node as usize;
-        if visited[node] {
-            continue;
-        }
+                // TODO: Contract nodes
+                for node in subgraph {
 
-        visited[node] = true;
-
-        // Traverse bidirectionally because we need weak comps
-        for i in graph.1[node].offset..graph.1[node + 1].offset {
-            let neighbor = graph.0[i as usize].to;
-            if !visited[neighbor as usize] {
-                stack.push(neighbor);
+                }
             }
         }
 
-        for i in incoming_graph.1[node].offset..incoming_graph.1[node + 1].offset {
-            let neighbor = incoming_graph.0[i as usize].to;
-            if !visited[neighbor as usize] {
-                stack.push(neighbor);
+    }
+
+    fn calc_shortcuts(node: u64, graph: &OffsetArray, incoming_graph: &OffsetArray) -> Vec<(u64, u64, u64, u64, u64)> {
+        let mut dijkstra = Dijkstra::new(graph);
+        // (from, to, weight, edge_id_a, edge_id_b)
+        let mut shortcuts : Vec<(u64, u64, u64, u64, u64)> = Vec::new();
+
+        for incoming_edge in incoming_graph.1[node as usize].offset..incoming_graph.1[node as usize + 1].offset {
+            let incoming_node = incoming_graph.0[incoming_edge as usize].to;
+            for outgoing_edge in graph.1[node as usize].offset..graph.1[node as usize + 1].offset {
+                let outgoing_node = graph.0[outgoing_edge as usize].to;
+                if let Some(shortest_path) = dijkstra.shortest_path(incoming_node, outgoing_node) {
+                    let direct_distance = incoming_graph.0[incoming_edge as usize].weight + graph.0[outgoing_edge as usize].weight;
+                    if shortest_path >= direct_distance {
+                        shortcuts.push((incoming_node, outgoing_node, direct_distance, incoming_edge, outgoing_edge));
+                    }
+                }
             }
         }
+
+        shortcuts
     }
-}
 
-fn calc_weakly_connected_comps(graph: &OffsetArray, incoming_graph: &OffsetArray) -> usize {
-    let mut num_comps = 0;
-    let mut visited = vec![false; graph.1.len() - 1];
+    fn calc_edges_deleted(node: u64, graph: &OffsetArray, incoming_graph: &OffsetArray) -> u64 {
+        let incoming_edges = incoming_graph.1[node as usize + 1].offset - incoming_graph.1[node as usize].offset;
+        let outgoing_edges = graph.1[node as usize + 1].offset - graph.1[node as usize].offset;
+        incoming_edges + outgoing_edges
+    }
 
-    for node in 0..visited.len() {
-        if !visited[node] {
-            num_comps += 1;
-            dfs(graph, incoming_graph, node as u64, &mut visited);
+    fn dfs(graph: &OffsetArray, incoming_graph: &OffsetArray, start: u64, visited: &mut [bool]) -> Vec<u64> {
+        let mut stack = Vec::new();
+        let mut subgraph = Vec::new();
+        stack.push(start);
+    
+        while let Some(node) = stack.pop() {
+            subgraph.push(node);
+            let node = node as usize;
+            if visited[node] {
+                continue;
+            }
+    
+            visited[node] = true;
+    
+            // Traverse bidirectionally because we need weak comps
+            for i in graph.1[node].offset..graph.1[node + 1].offset {
+                let neighbor = graph.0[i as usize].to;
+                if !visited[neighbor as usize] {
+                    stack.push(neighbor);
+                }
+            }
+    
+            for i in incoming_graph.1[node].offset..incoming_graph.1[node + 1].offset {
+                let neighbor = incoming_graph.0[i as usize].to;
+                if !visited[neighbor as usize] {
+                    stack.push(neighbor);
+                }
+            }
         }
+
+        subgraph
     }
-
-    num_comps
 }
-
 
 struct Dijkstra<'a> {
     graph: &'a OffsetArray,
@@ -447,51 +483,67 @@ fn read_query(filename: &str) -> Result<Vec<(u64, u64)>, Box<dyn Error>> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Question 1
+    // Task 1
+    if false {
+        // Load graph with CH levels
+        let mut log = BufWriter::new(File::create("dump.txt").expect("Could not create log"));
 
-    // Load graph with CH levels
-    let mut log = BufWriter::new(File::create("dump.txt").expect("Could not create log"));
+        let start = Instant::now();
+        println!("Started parsing...");
+        let (perm, graph, incoming_graph) = parse_graph("stgtregbz_ch.fmi")?;
+        let duration = start.elapsed();
+        println!("Loaded graph in {:.2?}", duration);
 
+        // Test Dijkstra vs CH
+        let mut dijkstra = Dijkstra::new(&graph);
+        // let s = 214733;
+        // let t = 429466;
+        let s = 377371;
+        let t = 754742;
+        print!("Dijkstra: ");
+        let start = Instant::now();
+        match dijkstra.shortest_path(perm[s as usize], perm[t as usize]) {
+            Some(dist) => print!("Found a shortest path from {s} to {t}: {dist} "),
+            None => print!("Did NOT find a path between {s} and {t} ")
+        }
+        let duration = start.elapsed();
+        println!("[{:.2?}]", duration);
+
+        let mut ch = CH::new(&graph, &incoming_graph);
+    
+        print!("CH (without stall-on-demand): ");
+        let start = Instant::now();
+        match ch.shortest_path(perm[s as usize], perm[t as usize], false) {
+            Some(dist) => print!("Found a shortest path from {s} to {t}: {dist} "),
+            None => print!("Did NOT find a path between {s} and {t} ")
+        }
+        let duration = start.elapsed();
+        println!("[{:.2?}]", duration);
+
+        print!("CH (with stall-on-demand): ");
+        let start = Instant::now();
+        match ch.shortest_path(perm[s as usize], perm[t as usize], true) {
+            Some(dist) => print!("Found a shortest path from {s} to {t}: {dist} "),
+            None => print!("Did NOT find a path between {s} and {t} ")
+        }
+        let duration = start.elapsed();
+        println!("[{:.2?}]", duration);
+    }
+    // Task 2: Run own CH preprocessing
+    
+    // TODO: IDK remove this or hmm
     let start = Instant::now();
     println!("Started parsing...");
-    let (perm, graph, incoming_graph) = parse_graph("stgtregbz_ch.fmi")?;
+    let (prep_perm, mut prep_graph, mut prep_incoming_graph) = parse_graph("inputs/MV.fmi")?;
     let duration = start.elapsed();
     println!("Loaded graph in {:.2?}", duration);
 
-    // Test Dijkstra vs CH
-    let mut dijkstra = Dijkstra::new(&graph);
-    // let s = 214733;
-    // let t = 429466;
-    let s = 377371;
-    let t = 754742;
-    print!("Dijkstra: ");
     let start = Instant::now();
-    match dijkstra.shortest_path(perm[s as usize], perm[t as usize]) {
-        Some(dist) => print!("Found a shortest path from {s} to {t}: {dist} "),
-        None => print!("Did NOT find a path between {s} and {t} ")
-    }
+    println!("Starting CH preprocessing...");
+    CH::preprocess(&mut prep_graph, &mut prep_incoming_graph, &prep_perm);
     let duration = start.elapsed();
-    println!("[{:.2?}]", duration);
+    println!("Preprocessed in {:.2?}", duration);
 
-    let mut ch = CH::new(&graph, &incoming_graph);
-   
-    print!("CH (without stall-on-demand): ");
-    let start = Instant::now();
-    match ch.shortest_path(perm[s as usize], perm[t as usize], false) {
-        Some(dist) => print!("Found a shortest path from {s} to {t}: {dist} "),
-        None => print!("Did NOT find a path between {s} and {t} ")
-    }
-    let duration = start.elapsed();
-    println!("[{:.2?}]", duration);
-
-    print!("CH (with stall-on-demand): ");
-    let start = Instant::now();
-    match ch.shortest_path(perm[s as usize], perm[t as usize], true) {
-        Some(dist) => print!("Found a shortest path from {s} to {t}: {dist} "),
-        None => print!("Did NOT find a path between {s} and {t} ")
-    }
-    let duration = start.elapsed();
-    println!("[{:.2?}]", duration);
 
     Ok(())
 }

2a8a3d29adc085016735573e767a0cf8d6b24e5c
JanTSV <jantheil2001@gmail.com>
2025-06-01 16:39:08 +0200

    naive query

--- movies/src/main.rs
+++ movies/src/main.rs
@@ -33,16 +33,45 @@ fn parse(filename: &str) -> Vec<Movie> {
         .collect()
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn naive<'a>(movies: &'a [Movie], query: &[String]) -> Vec<&'a Movie> {
+    println!("Starting naive()...");
+    let s = Instant::now();
+    let mut found = Vec::new();
 
-    if args.len() != 2 {
-        println!("Usage: ./movies <movies.txt>");
-        return;
+    'outer: for movie in movies {
+        let words: Vec<&str> = movie.description.split_whitespace().collect();
+
+        for q in query {
+            if !words.iter().any(|&w| w == q) {
+                continue 'outer; // If one word doesn't match, skip this movie
+            }
+        }
+
+        // All query words matched
+        found.push(movie);
     }
 
-    println!("Parsing {}...", args[1]);
+    println!("Ran naive() in {:.2?}", s.elapsed());
+    found
+}
+
+fn main() {
+    // let args: Vec<String> = env::args().collect();
+
+    // if args.len() != 2 {
+    //     println!("Usage: ./movies <movies.txt>");
+    //     return;
+    // }
+
+    // println!("Parsing {}...", args[1]);
     let s = Instant::now();
-    let movies = parse(&args[1]);
+    // let movies = parse(&args[1]);
+    let movies = parse("movies.txt");
     println!("Parsed in {:.2?}", s.elapsed());
+
+    let mut query = Vec::new();
+    query.push(String::from("Hello"));
+    query.push(String::from("Goodbye"));
+    let found = naive(&movies, &query);
+    dbg!(found);
 }

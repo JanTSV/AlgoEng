1c29d81c96a85e1f0a1df215f41b1577d6269907
JanTSV <jantheil2001@gmail.com>
2025-06-01 16:11:32 +0200

    parse movies

--- movies/src/main.rs
+++ movies/src/main.rs
@@ -1,3 +1,48 @@
+use std::{env, time::Instant};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Debug)]
+struct Movie {
+    title: String,
+    description: String
+}
+
+impl Movie {
+    fn new(title: String, description: String) -> Self {
+        Movie { title, description }
+    }
+}
+
+fn parse(filename: &str) -> Vec<Movie> {
+    let file = File::open(filename).expect("Could not open {filename}");
+    let reader = BufReader::new(file);
+
+    reader
+        .lines()
+        .map_while(Result::ok)
+        .map(|line| line.trim().to_string())
+        .filter(|line| !line.is_empty())
+        .filter_map(|line| {
+            let parts: Vec<&str> = line.split('\t').collect();
+            if parts.len() < 2 {
+                return None;
+            }
+            Some(Movie::new(parts[0].to_string(), parts[1].to_string()))
+        })
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: ./movies <movies.txt>");
+        return;
+    }
+
+    println!("Parsing {}...", args[1]);
+    let s = Instant::now();
+    let movies = parse(&args[1]);
+    println!("Parsed in {:.2?}", s.elapsed());
 }
